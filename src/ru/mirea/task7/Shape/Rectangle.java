f1d449b033c0f0538991a9a408fad8ed05b089a9
Эльдорадо <julia-julia01@mail.ru>
2021-09-22 12:03:28 +0300

    Fixed .gitignore

--- src/ru/mirea/task7/Shape/Rectangle.java
+++ src/ru/mirea/task7/Shape/Rectangle.java
@@ -3,4 +3,23 @@ package ru.mirea.task7.Shape;
 public class Rectangle extends Shape{
     protected double width;
     protected double length;
+    public Rectangle(){
+        width = 0;
+    }
+    public Rectangle(double radius){
+        this.radius = radius;
+    }
+    public Rectangle(double radius, String color, boolean filled){
+        super(color, filled);
+        this.radius = radius;
+    }
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+    public double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+    public String toString(){
+        return ("Circle, radius = "+radius+", colour = "+color+", filled - "+ filled);
+    }
 }
