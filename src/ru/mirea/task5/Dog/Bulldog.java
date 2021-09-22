4c151b81ec0f57d89a6b44d5122bdbd75c875514
Эльдорадо <julia-julia01@mail.ru>
2021-09-22 10:32:01 +0300

    Fixed .gitignore

---
+++ src/ru/mirea/task5/Dog/Bulldog.java
@@ -0,0 +1,21 @@
+package ru.mirea.task5.Dog;
+
+public class Bulldog extends Dog {
+    private String Bulldog;
+    public Bulldog(String n, int w, String b) {
+        super("Liza",20);
+        Bulldog=b;
+    }
+    public void setBulldog(String Bulldog) {
+        this.Bulldog = Bulldog;
+    }
+    public String getBulldog()
+    {
+        return Bulldog;
+    }
+    public String toString()
+    {
+        return getName()+", weight "+getWeight()+" , bulldog "+getBulldog();
+    }
+
+}
