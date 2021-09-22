dba3ed00494591e961cec33ead725ff15883c2a4
Эльдорадо <julia-julia01@mail.ru>
2021-09-22 10:22:04 +0300

    Fixed .gitignore

---
+++ src/ru/mirea/task1/op5/Sum.java
@@ -0,0 +1,41 @@
+package ru.mirea.task1.op5;
+
+public class Sum {
+    private int[] array;
+    private int sum;
+    public Sum(){
+    }
+    public void rand(){
+        this.array = new int[10];
+        for(int i = 0; i < array.length;i++)
+            array[i] = (int)(Math.random()*100);
+    }
+    public void setArray(){
+        for (int i = 0; i < array.length; i++)
+            System.out.print(" "+(array[i]));
+    }
+    public void For(){
+        sum = 0;
+        for(int i = 0; i < 10; i++)
+            sum+=array[i];
+    }
+    public void While(){
+        sum = 0;
+        int i = 0;
+        while (i<10) {
+            sum += array[i];
+            i++;
+        }
+    }
+    public void DoWhile(){
+        sum = 0;
+        int i = 0;
+         do{
+            sum += array[i];
+            i++;
+        }while (i<10);
+    }
+    public void getSum(){
+        System.out.println("Sum = " + sum);
+    }
+}
---
+++ src/ru/mirea/task1/op5/TestSum.java
@@ -0,0 +1,14 @@
+package ru.mirea.task1.op5;
+
+public class TestSum {
+    public static void main(String[] ars){
+        Sum sum = new Sum();
+        sum.rand();
+        sum.For();
+        sum.getSum();
+        sum.While();
+        sum.getSum();
+        sum.DoWhile();
+        sum.getSum();
+    }
+}
